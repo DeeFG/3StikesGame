0420e6dd8bf4d10f2723f891b71cc34e653c3f8a
DeeFG <45301109+DeeFG@users.noreply.github.com>
2019-05-24 19:41:32 -0400

    js file created
    some variables and images added

---
+++ app.js
@@ -0,0 +1,23 @@
+let doorImage1 = document.getElementById('door1');
+const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
+let doorImage2 = document.getElementById('door2') ;
+let doorImage3 = document.getElementById('door3');
+beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
+spaceDoorPath="https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
+
+
+
+door1.onclick = () => {
+  // use #id to reference  and change image URL
+	 doorImage1.src = botDoorPath;
+}
+
+door2.onclick = () => {
+ 
+  doorImage2.src = beachDoorPath;
+  
+}
+
+door3.onclick = () => {
+  doorImage3.src = spaceDoorPath; 
+}
